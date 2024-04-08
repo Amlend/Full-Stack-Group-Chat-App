a49867e369ca26ad2fbd32a33835f7aebda0cfd6
Amlend <amlendsingh@gmail.com>
2024-04-08 15:14:58 +0530

    login-signup

---
+++ controllers/signUp.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const User = require("../models/user");
+
+exports.getSignUpPage = (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "public", "views", "signUp.html"));
+};
+exports.getSignUpSuccess = (req, res, next) => {
+  res.sendFile(
+    path.join(__dirname, "../", "public", "views", "successRegister.html")
+  );
+};
+
+exports.postUserData = (req, res, next) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password,
+  };
+
+  User.create({
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    password: user.password,
+  })
+    .then(() => {
+      res.redirect("/successful-registered");
+      console.log("New User Come..");
+    })
+    .catch((err) => {
+      res.status(200).send("Email Already exist, use different email");
+    });
+};
---
+++ routers/singUp.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const signUpController = require("../controllers/signUp");
+
+const router = express.Router();
+
+router.get("/signUp", signUpController.getSignUpPage);
+router.post("/register-user", signUpController.postUserData);
+router.get("/successful-registered", signUpController.getSignUpSuccess);
+
+module.exports = router;
